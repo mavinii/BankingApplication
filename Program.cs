85368ea2cc95a4bd0d43137d54ac4b2418b09f6d
mavinii <pg.marcosoliveira@icloud.com>
2021-10-22 23:56:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BankingApplication.Models;
+using System;
+
+//22931 - Marcos Oliveira
+namespace BankingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Banking Application ****");
+            Console.WriteLine("");
+
+            Driver driver = new Driver();
+        }
+    }
+}
